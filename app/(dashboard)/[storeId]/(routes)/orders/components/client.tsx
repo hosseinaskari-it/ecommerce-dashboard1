1bc7be19ec2fcc8c7194f5bb0ff6237763f4f3ad
Hossein Askari <hosseinaskari.it@gmail.com>
2024-02-18 11:46:21 +0330

    Add all my files

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+interface OrderClientProps {
+  data: OrderColumn[];
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+  return (
+    <>
+      <Heading
+        title={`Billboards (${data.length})`}
+        description="Manage orders for your store"
+      />
+
+      <Separator />
+      <DataTable searchkey="label" columns={columns} data={data} />
+      <ApiList entityName="products" entityIdName="productsId" />
+    </>
+  );
+};
